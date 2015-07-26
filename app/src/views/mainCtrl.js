d1c30e150238e8389aa51b3cb2558e8a9d1c2b52
brunion1 <brunion1@gmail.com>
2015-07-26 11:35:15 -0500

    Changes for Cards
    And stuff

--- app/src/views/mainCtrl.js
+++ app/src/views/mainCtrl.js
@@ -3,6 +3,17 @@ app.controller('mainCtrl', function($scope, $http, $mdSidenav, $mdUtil, $mdMedia
 
     $scope.stories = [];
     loadStories();
+    $scope.listMode = true;
+    
+
+    $scope.$watch('listMode', function(){
+        console.log("Value is now: " + $scope.listMode);
+        if($scope.listMode){
+            $scope.mode = "List";
+        }else{
+            $scope.mode = "Card";
+        }
+    }, true);
     
     function loadStories(){
       $http.get('http://www.reddit.com/r/funny/new.json?sort=new')
@@ -10,6 +21,7 @@ app.controller('mainCtrl', function($scope, $http, $mdSidenav, $mdUtil, $mdMedia
         angular.forEach(response.data.children, function(child){
           $scope.stories.push(child.data);
         });
+      console.log($scope.stories);
       })
     }
     
